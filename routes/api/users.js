5d6cdd4f487a9fbbb58b091c1bcc6297387b6fc4
Eric D. Torres <etorresnotary@gmail.com>
2020-10-19 12:52:53 -0500

    routes folder with routes and config folder

---
+++ routes/api/users.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const express = require("express");
+const router = express.Router();
+
+//@route        GET api/users
+//@desc         Test route
+//@access       Public
+router.get("/", (req, res) => res.send("Usera route"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = ("express");
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//connect to DB
+connectDB();
+
+//define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 PORT = process.env.PORT || 46723;
 
