62610e5e7a950d190d5885358ea5263da5f393ae
Eric D. Torres <etorresnotary@gmail.com>
2020-10-20 16:21:26 -0500

    add comment to a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -157,4 +157,39 @@ router.put("/unlike/:id", auth, async (req, res) => {
   }
 });
 
+//@route        POST api/posts/comment/:id
+//@desc         Create a comment on a post
+//@access       Private
+router.post(
+    "/comment/:id",
+    [auth, check("text", "Text is required to comment").not().isEmpty()],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
+        const user = await User.findById(req.user.id).select("-password");
+        const post = await Post.findById(req.user.id);
+
+        const newComment = {
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id,
+        };
+  
+        post.comments.unshift(newComment);
+
+        await post.save();
+  
+        res.json(post.comments);
+      } catch (err) {
+        console.error(err.message);
+        res.send(500).send("server error");
+      }
+    }
+  );
+
 module.exports = router;
