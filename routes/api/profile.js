19f9ecc62f3899fdfd5c91b9a751e16442e9ff8e
Eric D. Torres <etorresnotary@gmail.com>
2020-10-20 09:24:43 -0500

    profile api routes established

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -35,26 +35,73 @@ router.post(
   "/",
   [
     auth,
-    [check("status", "Status is a required field").not().isEmpty()],
-    check("skills", "Skills is a required field").not().isEmpty(),
+    [
+      check("status", "Status is a required field").not().isEmpty(),
+      check("skills", "Skills is a required field").not().isEmpty(),
+    ],
   ],
   async (req, res) => {
-      const errors = validationResult(req);
-      if(!errors.isEmpty()) {
-          return res.status(400).json({errors: errors.array()});
-      }
-    try {
-      const profile = await (
-        await Profile.findOne({ user: req.user.id })
-      ).populated("user", ["name", "avatar"]);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-      if (!profile) {
-        res
-          .status(400)
-          .json({ msg: "There is no profile available for this user" });
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    //build social object
+    profileFields.social = {};
+
+    if (youtube) profileFields.social.youtube - youtube;
+    if (twitter) profileFields.social.twitter - twitter;
+    if (facebook) profileFields.social.facebook - facebook;
+    if (linkedin) profileFields.social.linkedin - linkedin;
+    if (instagram) profileFields.social.instagram - instragram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        //update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return res.json(profile);
       }
+
+      profile = new Profile(profileFields);
+
+      await Profile.save();
 
       res.json(profile);
+
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
