445d9ab94bd1e805de0d08739fd45e1e0cc33246
Eric D. Torres <etorresnotary@gmail.com>
2020-10-20 15:59:38 -0500

    posts like functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -96,8 +96,28 @@ router.delete("/:id", auth, async (req, res) => {
       if (err.kind === "ObjectId") {
         return res.status(404).json({ msg: "Post not found" });
       }
-      res.send(500).send("server error");
+      res.status(500).send("server error");
     }
   });
+
+//@route        PUT api/posts/like/:id
+//@desc         Like a post
+//@access       Private
+router.put("/like/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        //check if post has already been liked by this user
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.json(400).json({ msg: "Post already liked by this user"});
+        }
+        post.likes.unshift({user: req.user.id});
+        await post.save();
+
+        res.json(post.likes)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error")
+    }
+})
 
 module.exports = router;
