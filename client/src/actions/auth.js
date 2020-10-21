60ee1d02fc2a09fdf06bb89781379d3a9ce6a3f7
Eric D. Torres <etorresnotary@gmail.com>
2020-10-21 17:20:20 -0500

    server side validation working

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -23,7 +23,7 @@ export const register = ({ name, email, password }) => async (dispatch) => {
     const errors = err.response.data.errors;
 
     if (errors) {
-      errors.foreach((err) => dispatch(setAlert(err.msg, "danger")));
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
     }
     dispatch({
       type: REGISTER_FAIL,
