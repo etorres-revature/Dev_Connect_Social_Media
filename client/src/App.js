55f180a6d25e9f33d371df145dd87a3eed67b4b5
Eric D. Torres <etorresnotary@gmail.com>
2020-10-21 11:28:59 -0500

    adding React to project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => 
+   <Fragment>
+
+     <h1>Welcom to the app...</h1>
+     
+   </Fragment>
+
+
+export default App;
