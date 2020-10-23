2b729dca7dcc5da0d3fa3de6d8e84258e49cf795
Eric D. Torres <etorresnotary@gmail.com>
2020-10-23 09:12:10 -0500

    logic to display in dashboard for if user not set up profile yet

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
@@ -20,7 +21,17 @@ const Dashboard = ({
       <p className="lead">
         <i className="fas fa-user"></i> Welcome {user && user.name}
       </p>
-      {profile !== null ? <Fragment>has</Fragment> : <Fragment>has not</Fragment>}
+      {profile !== null ? (
+        <Fragment>has</Fragment>
+      ) : (
+        <Fragment>
+          <p>You have not set up your User Profile.</p>
+          <p> Please add your information.</p>
+          <Link to="/create-profile" className="btn btn-primary my-1">
+            Create User Profile
+          </Link>
+        </Fragment>
+      )}
     </Fragment>
   );
 };
