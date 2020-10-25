3ea38d2542c88874befa254fb1c8bcbcb117ab83
Eric D. Torres <etorresnotary@gmail.com>
2020-10-24 19:36:05 -0500

    display user profiles of all devs

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import Spinner from "../layout/Spinner";
+import ProfileItem from "./ProfileItem";
 import { connect } from "react-redux";
 import { getProfiles } from "../../actions/profile";
 
@@ -8,7 +9,31 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
   }, []);
-  return <div></div>;
+
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className="large text-primary">Developers</h1>
+          <p className="lead">
+            <i className="fab fa-connectdevelop"></i> Browse and Connect with
+            other Developers
+          </p>
+          <div className="profiles">
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h3>No profiles found ...</h3>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
