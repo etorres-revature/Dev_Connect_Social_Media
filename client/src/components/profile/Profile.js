5e0826f3b70a8a74e1fc0dae62bf3003f33f46b6
Eric D. Torres <etorresnotary@gmail.com>
2020-10-24 20:10:24 -0500

    view profile from developers page working

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,32 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import { getProfileByID } from "../../actions/profile";
+import auth from "../../reducers/auth";
+
+const Profile = ({
+  getProfileByID,
+  profile: { profile, loading },
+  auth,
+  match,
+}) => {
+  useEffect(() => {
+    getProfileByID(match.params.id);
+  }, [getProfileByID]);
+
+  return <div>profile</div>;
+};
+
+Profile.propTypes = {
+  getProfileByID: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { getProfileByID })(Profile);
